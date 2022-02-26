c173350a02ff4377666991afb57f479bd34f509c
Sally <81373766+sallypang626@users.noreply.github.com>
2022-02-26 14:51:39 +0800

    google map with 2 marker

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 function initMap() {
     // The location of Uluru
-    const hkccwk = { lat: 22.312584, lng: 114.165328 };
+    const hkccWK = { lat: 22.312584, lng: 114.165328 };
+    const hkccHHB = { lat: 22.30346025, lng: 114.18473645238535 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: hkccwk,
+      zoom: 14,
+      center: hkccWK,
     });
+    //add marker function 
+    addMarker(hkccWK);
+    addMarker(hkccHHB);
+    function addMarker(coords){
+        const marker = new google.maps.Marker({
+            position: coords,
+            map: map,
+            });
+    }
+    
     // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: hkccwk,
-      map: map,
-    });
+
   }
